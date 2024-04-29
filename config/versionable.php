91180f6263cb7787c6d73787db9695ee35728087
Marco Panzanaro <marco.panzanaro@willbit.com>
2024-04-29 23:23:00 +0200

    feat(configs): imbastiti config files

---
+++ config/versionable.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+     * The version model to use.
+     * Feel free to change this, if you need specific version
+     * model logic.
+     */
+    'version_model' => Modules\Core\App\Models\Version::class,
+];
