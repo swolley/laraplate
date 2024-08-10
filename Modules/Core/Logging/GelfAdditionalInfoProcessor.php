f38132ba152269943b0ff035647f1e41372f54a6
Marco Panzanaro <marco.panzanaro@willbit.com>
2024-08-10 21:31:59 +0200

    feat(gelf): gelf logger custom classes

--- Modules/Core/Logging/GelfAdditionalInfoProcessor.php
+++ Modules/Core/Logging/GelfAdditionalInfoProcessor.php
@@ -5,27 +5,29 @@ namespace Modules\Core\Logging;
 use Monolog\LogRecord;
 use Illuminate\Support\Facades\Auth;
 use Monolog\Processor\ProcessorInterface;
-// use Monolog\Processor\IntrospectionProcessor;
 use Monolog\Processor\PsrLogMessageProcessor;
 
 class GelfAdditionalInfoProcessor implements ProcessorInterface
 {
-	// private IntrospectionProcessor $introspectionProcessor;
 	private PsrLogMessageProcessor $psrLogMessageProcessor;
+	private $channel;
 
-	public function __construct()
+	public function __construct(?string $channel = null)
 	{
-		// 	$this->introspectionProcessor = new IntrospectionProcessor(Level::Error, [], 4);
+		$this->channel = $channel;
 		$this->psrLogMessageProcessor = new PsrLogMessageProcessor(removeUsedContextFields: true);
 	}
 
 	public function __invoke(LogRecord $record): LogRecord
 	{
-		// $this->introspectionProcessor->__invoke($record);
 		$record = $this->psrLogMessageProcessor->__invoke($record);
 
-		$extra = ['user' => Auth::user()->username ?? 'anonymous', 'application_name' => config('app.name')];
-		$record->extra += $extra;
+		$extra = [
+			'user' => Auth::user()->username ?? 'anonymous',
+			'application_name' => config('app.name'),
+			'channel' => $this->channel ?? config('logging.default'),
+		];
+		$record->extra = array_merge($record->extra, $extra);
 
 		return $record;
 	}
