674547345f1017c9ee7902242dec02336c3a751c
Swolley <myentity@inwind.it>
2025-04-07 00:37:28 +0200

    chore: bump version to 1.6.4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,18 +11,5 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // $modules = modules(prioritySort: true);
-        // foreach ($modules as $module) {
-        // 	$module_dir = module_path($module, 'database/seeders');
-        // 	if (is_dir($module_dir)) {
-        // 		$seeders = glob($module_dir . '/*.php');
-        // 		foreach ($seeders as $seeder) {
-        // 			$seeder_class = "Modules\\$module\\Database\\Seeders\\" . basename($seeder, '.php');
-        // 			$this->call($seeder_class);
-        // 		}
-        // 	}
-        // }
-    }
+    public function run(): void {}
 }
