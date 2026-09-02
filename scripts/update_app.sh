61a9f22b045d19c56fe634b89dfcd98f5483c169
swolley <myentity@inwind.it>
2026-09-02 23:09:30 +0200

    Update submodules and dependencies; enhance update script
    - Updated submodule references for CMS and Core modules.
    - Updated composer.lock to reflect new versions of dependencies, including:
      - filament packages from v5.7.7 to v5.7.8
      - laravel/framework from v12.68.0 to v12.69.1
      - laravel/socialite from v5.30.1 to v5.31.0
      - league/flysystem from v3.35.3 to v3.36.0
      - monolog/monolog from v3.10.0 to v3.11.0
      - neuron-core/neuron-ai from v3.16.7 to v3.16.8
      - spatie/laravel-medialibrary from v11.23.5 to v11.23.6
      - larastan/larastan from v3.10.0 to v3.11.0
      - rector/rector from v2.6.5 to v2.6.6
    - Updated package-lock.json for various @rolldown bindings to version 1.2.7.
    - Modified update_app.sh script to provide more informative output during submodule updates and added user prompt for full update confirmation.

--- scripts/update_app.sh
+++ scripts/update_app.sh
@@ -4,18 +4,26 @@ if [[ "$PWD" == *"scripts"* ]]; then
    cd ..
 fi
 
-git submodule foreach 'git pull'
+git submodule foreach --recursive '
+    before=$(git describe --tags --abbrev=0 HEAD 2>/dev/null || echo "no tag") 
+    git pull
+    after=$(git describe --tags --abbrev=0 HEAD 2>/dev/null || echo "no tag")
+    if [ "$before" != "$after" ]; then
+        echo "from $before to $after"
+    fi
+    echo ""
+'
 
+echo "Back to Laraplate"
 before=$(git describe --tags --abbrev=0 HEAD 2>/dev/null || echo "no tag")
-
 git pull
-
 after=$(git describe --tags --abbrev=0 HEAD 2>/dev/null || echo "no tag")
+[ "$before" != "$after" ] && echo "from $before to $after" || echo""
 
-if [ "$before" != "$after" ]; then
-    message="Updated from $before to $after"
-    echo $message
-    echo "[$(date +%Y-%m-%d\ %H:%M:%S)] $message" >> update_app.log
+read -p "Do you want to run the full update? (y/N): " choice
+
+if [[ ! "$choice" =~ ^[Yy]$ ]]; then
+    # echo "Update aborted."
     exit 0
 fi
 
